655d07cc3bc48c8b64112da8e5a7f6f40d86950b
Nandiwaleamol <nandivaleamol@gmail.com>
2024-12-09 18:44:08 +0530

    singleton

--- src/singleton/LazySingleton.java
+++ src/singleton/LazySingleton.java
@@ -1,4 +1,10 @@
 package singleton;
 
 public class LazySingleton {
+
+    private static LazySingleton instance = null;
+
+    private LazySingleton() {
+        // Private constructor to prevent instantiation from outside the class
+    }
 }
